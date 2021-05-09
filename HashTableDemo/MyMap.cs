8ba61ceda63751944da0cd6016394d637c25899d
Prashant Bhure <Prashantbhure50@gmail.com>
2021-05-09 21:27:19 +0530

    Add project files.

---
+++ HashTableDemo/MyMap.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTableDemo
+{
+    class MyMap<K,V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K,V>>[] items;
+        public MyMap(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkkedList)
+            {
+                if (item.key.Equals(key))
+                {
+                    return item.value;
+                }
+            }
+            return default(V);
+        }
+        protected LinkedList<KeyValue<K,V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if(linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { key = key, value = value };
+            linkedList.AddLast(item);
+        }
+       
+        
+
+    }
+    public struct KeyValue<K, V>
+    {
+        public K key { get; set; }
+        public V value { get; set; }
+    }
+}
---
+++ HashTableDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HashTableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMap<string ,string> hash = new MyMap<string,string>(5);
+            hash.Add("0","To");
+            hash.Add("1","be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string result = hash.Get("0");
+            Console.WriteLine(result);
+        }
+    }
+}
